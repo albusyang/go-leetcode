3f2f65523a5ff6c49e6c49f6de47d0408065c1ec
Albus <albusyang@outlook.com>
2019-07-15 21:29:55 +0800

    add no.169 & no.229

---
+++ 15thWeek/169.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(majorityElement([]int{2,2,1,1,1,2,2}))
+}
+
+func majorityElement(nums []int) int {
+	result := 0
+	rmap := make(map[int]int)
+	for _, v := range nums {
+		_, ok := rmap[v]
+		if ok {
+			rmap[v] = rmap[v]+1
+		} else {
+			rmap[v] = 1
+		}
+	}
+	for k := range rmap {
+		if rmap[k] > len(nums)/2 {
+			result = k
+		}
+	}	
+	return result
+}
