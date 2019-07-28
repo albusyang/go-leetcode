ec5055825d12c0b2fca560726a4cc7e011902188
Albus <albusyang@outlook.com>
2019-07-28 19:11:09 +0800

    add no.121

---
+++ 17thWeek/121.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(maxProfit([]int{7,1,5,3,6,4}))
+}
+
+func maxProfit(prices []int) int {
+	result := 0
+	
+	for i := 0; i < len(prices); i++ {
+		profit := 0
+		for j := i+1; j < len(prices); j++ {
+			
+			if prices[j]-prices[i] > profit {
+				profit = prices[j]-prices[i]
+			}
+		}
+		if profit > result {
+			result = profit
+		}
+	}
+	
+	return result
+}
