62ff1cdc329d7c85a05e416d45fa3cf798ec0722
albusyang <albusyang@outlook.com>
2021-05-06 17:52:53 +0800

    #43 add jianzhi_offer_64

---
+++ 2021/week01/jianzhi_offer_64_test.go
@@ -0,0 +1,14 @@
+package week01
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_sumNums(t *testing.T) {
+	fmt.Println(sumNums(10000))
+}
+
+func Test_sumNums1(t *testing.T) {
+	fmt.Println(sumNums1(10000))
+}
