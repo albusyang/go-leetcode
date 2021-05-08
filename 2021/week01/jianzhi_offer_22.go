102a25fd8b68b13eeee89fa464597c7f7562f2ce
albusyang <albusyang@outlook.com>
2021-05-08 13:40:45 +0800

    #43 添加第三种解法 getKthFromEnd2

--- 2021/week01/jianzhi_offer_22.go
+++ 2021/week01/jianzhi_offer_22.go
@@ -7,6 +7,10 @@ package week01
  *     Next *ListNode
  * }
  */
+
+// 先拿到链表长度；
+//
+// 倒数第k位相当于 正数的链表长度减k位
 func getKthFromEnd(head *ListNode, k int) *ListNode {
 	if head == nil || k == 0 {
 		return head
@@ -39,6 +43,23 @@ func getKthFromEnd1(head *ListNode, k int) *ListNode {
 	}
 	if k >= 0 && k <= lenListNode {
 		myHead = nodeArr[lenListNode-k]
+	}
+	return myHead
+}
+
+// 直接两个指针，第一个先走k位，然后两个一起走，第一个到nil时返回第二个
+func getKthFromEnd2(head *ListNode, k int) *ListNode {
+	kHead := head
+	myHead := head
+	for i := 0; i < k; i++ {
+		if kHead == nil {
+			break
+		}
+		kHead = kHead.Next
+	}
+	for kHead != nil {
+		myHead = myHead.Next
+		kHead = kHead.Next
 	}
 	return myHead
 }
--- 2021/week01/jianzhi_offer_22_test.go
+++ 2021/week01/jianzhi_offer_22_test.go
@@ -46,3 +46,24 @@ func Test_getKthFromEnd1(t *testing.T) {
 
 	fmt.Println(r)
 }
+
+func Test_getKthFromEnd2(t *testing.T) {
+	// n := 10
+	// for i := 0; i < n; i++ {
+	node5 := &ListNode{5, nil}
+	node4 := &ListNode{4, node5}
+	node3 := &ListNode{3, node4}
+	node2 := &ListNode{2, node3}
+	node1 := &ListNode{1, node2}
+	node0 := &ListNode{0, node1}
+	// }
+	n := node0
+	for n != nil {
+		fmt.Println(n)
+		n = n.Next
+	}
+
+	r := getKthFromEnd2(node0, 3)
+
+	fmt.Println(r)
+}
