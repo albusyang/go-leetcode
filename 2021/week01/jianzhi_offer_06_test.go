54d95621431e520350de8ff9f43dcd43541e4190
Albus Yang <albus@AlbusdeMacBook-Pro.local>
2021-05-19 01:48:10 +0800

    add jianzhi_offer_25

--- 2021/week01/jianzhi_offer_06_test.go
+++ 2021/week01/jianzhi_offer_06_test.go
@@ -6,15 +6,12 @@ import (
 )
 
 func Test_reversePrint(t *testing.T) {
-	// n := 10
-	// for i := 0; i < n; i++ {
 	node5 := &ListNode{5, nil}
 	node4 := &ListNode{4, node5}
 	node3 := &ListNode{3, node4}
 	node2 := &ListNode{2, node3}
 	node1 := &ListNode{1, node2}
 	node0 := &ListNode{0, node1}
-	// }
 	n := node0
 	for n != nil {
 		fmt.Println(n)
